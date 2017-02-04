d7147b82734272ae19996e7d359b750190756135
Scarlett Clark <sgclark@kde.org>
2017-02-04 10:17:44 -0700

    make again

--- functions/build-firefox.sh
+++ functions/build-firefox.sh
@@ -46,7 +46,8 @@ EOF
 ./mach configure
 
 cp ../plasmazilla/MozillaFirefox/kde.js /app/src/firefox-50.1.0/firefox-build-dir/dist/bin/defaults/pref/
-/usr/bin/make -f client.mk install
+/usr/bin/make -f client.mk build
+make install
 
 
 cat > /opt/usr/share/applications/firefox.desktop << EOF
